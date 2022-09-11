fbfd9272b57ca228e80a1f893ee5f8ee16daf550
Abhijit Apotikar <abhijit.apotikar@think360.ai>
2022-09-11 19:33:45 +0530

    Handle invalid user using useState()

--- react-form-assignment/src/pages/login.js
+++ react-form-assignment/src/pages/login.js
@@ -5,16 +5,42 @@ import users from "../constants/users.js";
 
 function LoginPage() {
   let navigate = useNavigate();
+  var [username, setUsername] = useState("");
+  var [password, setPassword] = useState("");
+
+  function onChangeUsername(e) {
+    setUsername(e.target.value);
+  }
+
+  function onChangePassword(e) {
+    setPassword(e.target.value);
+  }
+
+  function showAlert() {
+    alert("Wrong username or password\nPlease retry.");
+    // document.getElementsByClassName("input-label").value = "";
+  }
+
+  function checkValidUser(username, password) {
+    let i;
+    for (i = 0; i < users.length; i++) {
+      if (users[i].username == username && users[i].password == password) {
+        return true;
+      }
+    }
+    return false;
+  }
 
   function onSubmit(e) {
     e.preventDefault();
     const userName = e.target[0].value;
     const password = e.target[1].value;
-    let i;
-    for (i = 0; i < users.length; i++) {
-      if (users[i].username == userName && users[i].password == password) {
-        navigate("/personalDetailsPage");
-      }
+    if (checkValidUser(userName, password)) {
+      navigate("/personalDetailsPage");
+    } else {
+      showAlert();
+      setUsername("");
+      setPassword("");
     }
   }
 
@@ -30,11 +56,25 @@ function LoginPage() {
         <form className="log-in-form" onSubmit={onSubmit}>
           <label>Username :</label>
           <br />
-          <input type="text" className="input-label" />
+          <input
+            type="text"
+            size={15}
+            value={username}
+            onChange={onChangeUsername}
+            className="input-label"
+            required
+          />
           <br />
           <label>Password :</label>
           <br />
-          <input type="text" className="input-label" />
+          <input
+            type="password"
+            size={15}
+            value={password}
+            onChange={onChangePassword}
+            className="input-label"
+            required
+          />
           <br />
           <div className="submit-btn-container">
             <button type="submit">Log In</button>
